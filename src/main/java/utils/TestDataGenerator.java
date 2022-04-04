0bd24479f3059cf56bface6e03dae9f7bf270e3d
Fatih <fatih.topcuoglu@persgroep.net>
2022-04-04 11:56:12 +0200

    v4 - readme.MD update - TEST_CASE2 added

--- src/main/java/utils/TestDataGenerator.java
+++ src/main/java/utils/TestDataGenerator.java
@@ -1,81 +1,23 @@
 package utils;
 
-import com.fasterxml.uuid.Generators;
-
-import java.sql.Timestamp;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-import java.util.Random;
-
 public class TestDataGenerator {
-    private static final Random rand = new Random();
-    public static final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    public static final int MAX_RANDOM = 10000;
-    public static final int LENGTH = 20;
+
 
     private TestDataGenerator() {
     }
 
 
     public static String generateDrink() {
-//  Below is an example. Please write a method which generates dynamically random names
-//  You could for example use 'StringBuilder()'
-
-        String drinkName = "Something";
-        return drinkName;
+        return "";
     }
 
 
     public static String generateRandomEmail() {
-//  Below is an example. Please write a method which generates dynamically random emails
-        return "test@gmail.com";
+        return "";
     }
 
     public static int generateNextInt(){
-        Random random = new Random();
-        int bound = 10;
-        int i = random.nextInt(bound);
-        return i;
+        return 1;
     }
-
-//    public static long number() {
-//        var offset = rand.nextInt() * 1000 + 1;
-//        return System.currentTimeMillis() + offset;
-//    }
-//
-//
-//    public static String generateEncodedEmail() {
-//        var MAIL = generateRandomEmail();
-//        return MAIL.replaceAll("\\+", "%2B");
-//    }
-//
-//    //futureDateWithDays - aanpassen
-//    public static String futureDate(int days, String format) {
-//        var now = LocalDateTime.now();
-//        var date = now.plusDays(days);
-//        var dateTimeFormatter = DateTimeFormatter.ofPattern(format);
-//        return dateTimeFormatter.format(date);
-//    }
-//
-//    //    Kijken of deze weg kan of hernoemen naar 'now'
-//    public static LocalDate currentDate() {
-//        return LocalDate.now();
-//    }
-//
-//
-//    public static String generateUUID() {
-//        var uuid1 = Generators.timeBasedGenerator().generate();
-//        return uuid1.toString();
-//    }
-//
-//    public static String generateTimeStamp() {
-//        return new Timestamp(LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)).toString();
-//    }
-//
-//    public static int generateRandomInt() {
-//        return rand.nextInt(MAX_RANDOM - 1) + 1;
-//    }
 
 }
--- src/test/java/unitTest/UnitTests.java
+++ src/test/java/unitTest/UnitTests.java
@@ -13,62 +13,19 @@ import java.time.format.DateTimeFormatter;
 public class UnitTests {
     @Test
     public void testRandomDrink() {
-        var drinkName = TestDataGenerator.generateDrink();
-        Assert.assertEquals(drinkName, "Something");
+
     }
 
     @Test
     public void testRandomEmail() {
-        var email = TestDataGenerator.generateRandomEmail();
-        Assert.assertEquals(email, "test@gmail.com");
+
     }
 
 
     @Test
     public void testRandomInt() {
-        var number = TestDataGenerator.generateNextInt();
-        Assert.assertTrue(number <= 10);
+
     }
-
-
-//    @Test
-//    public void testCurrentDate() {
-//        var firstDate = TestDataGenerator.currentDate();
-//        var secondDate = firstDate.minusDays(1);
-//        Assert.assertEquals(secondDate, firstDate.minusDays(1));
-//    }
-//
-//    @Test
-//    public void testFutureDate() {
-//        var futureDateOne = TestDataGenerator.futureDate(1, "yyyy-MM-dd");
-//        var futureDateTwo = TestDataGenerator.futureDate(3, "yyyy-MM-dd");
-//        var futureDateNow = TestDataGenerator.futureDate(0, "yyyy-MM-dd");
-//
-//        Assert.assertNotEquals(futureDateOne, futureDateTwo);
-//        Assert.assertEquals(futureDateNow, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-//    }
-//
-//
-//
-//    @Test
-//    public void testEncodedEmail() {
-//        var encodedMail = TestDataGenerator.generateEncodedEmail();
-//        Assert.assertTrue(encodedMail.contains("%2B"));
-//    }
-//
-//
-//
-//    @Test
-//    public void testUUIDGenerator() {
-//        var uuId = TestDataGenerator.generateUUID();
-//        Assert.assertTrue(uuId.contains("-"));
-//    }
-//
-//    @Test
-//    public void testTimeStampGenerator() {
-//        var ts = TestDataGenerator.generateTimeStamp();
-//        Assert.assertTrue(ts.contains(":"));
-//    }
 
 
 
