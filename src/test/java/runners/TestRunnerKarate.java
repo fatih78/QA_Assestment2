8db3ac01f7b296fdeb7bba10bbce70833175fce7
dmitry_malinovsky <dmitrii.malinovschii@gmail.com>
2022-04-23 21:35:38 +0200

    Added common test runner
    - Added test runner functionality
    - Added test report functionality
    - Added test report sample

--- src/test/java/runners/TestRunnerKarate.java
+++ src/test/java/runners/TestRunnerKarate.java
@@ -1,14 +1,20 @@
 package runners;
 
-
 import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+
+import static org.junit.Assert.*;
+
 
 public class TestRunnerKarate {
     public static Results results;
 
     public static void testParallel() {
-        //TODO
-        //write your code here
+        Results results = Runner.path("classpath:features")
+                .outputCucumberJson(true)
+                .parallel(1);
+
+        assertEquals(results.getErrorMessages(), 0, results.getFailCount());
     }
 
     public static void main(String[] args) {
